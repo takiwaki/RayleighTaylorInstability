b4efe7f7aed51915c902dcafb29d2940b6d96c20
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2026-02-11 15:18:36 +0900

    update

--- BoxMultiRT3D/src/boundary.f90
+++ BoxMultiRT3D/src/boundary.f90
@@ -2,6 +2,14 @@ module boundarymod
   use basicmod
   implicit none
   private
+
+  integer,parameter:: periodicb=1,reflection=2,outflow=3
+  integer,parameter:: boundary_xin=reflection, boundary_xout=reflection
+  integer,parameter:: boundary_yin=periodicb  , boundary_yout=periodicb
+  integer,parameter:: boundary_zin=periodicb  , boundary_zout=periodicb
+  
+  integer,parameter:: nv1=3,nv2=4,nv3=5
+ 
 !!  real(8),dimension(mgn,jn,kn,nbc):: varsendXstt,varsendXend
 !!  real(8),dimension(in,mgn,kn,nbc):: varsendYstt,varsendYend
 !!  real(8),dimension(in,jn,mgn,nbc):: varsendZstt,varsendZend
@@ -37,9 +45,9 @@ contains
   do i=1,mgn
      varsendXend(i,j,k,1) =  d(ie-mgn+i,j,k)
      varsendXend(i,j,k,2) = ei(ie-mgn+i,j,k)
-     varsendXend(i,j,k,3) = v1(ie-mgn+i,j,k)
-     varsendXend(i,j,k,4) = v2(ie-mgn+i,j,k)
-     varsendXend(i,j,k,5) = v3(ie-mgn+i,j,k)
+     varsendXend(i,j,k,nv1) = v1(ie-mgn+i,j,k)
+     varsendXend(i,j,k,nv2) = v2(ie-mgn+i,j,k)
+     varsendXend(i,j,k,nv3) = v3(ie-mgn+i,j,k)
      varsendXend(i,j,k,6) = b1(ie-mgn+i,j,k)
      varsendXend(i,j,k,7) = b2(ie-mgn+i,j,k)
      varsendXend(i,j,k,8) = b3(ie-mgn+i,j,k)
@@ -47,9 +55,9 @@ contains
 
      varsendXstt(i,j,k,1) =  d(  is+i-1,j,k)
      varsendXstt(i,j,k,2) = ei(  is+i-1,j,k)
-     varsendXstt(i,j,k,3) = v1(  is+i-1,j,k)
-     varsendXstt(i,j,k,4) = v2(  is+i-1,j,k)
-     varsendXstt(i,j,k,5) = v3(  is+i-1,j,k)
+     varsendXstt(i,j,k,nv1) = v1(  is+i-1,j,k)
+     varsendXstt(i,j,k,nv2) = v2(  is+i-1,j,k)
+     varsendXstt(i,j,k,nv3) = v3(  is+i-1,j,k)
      varsendXstt(i,j,k,6) = b1(  is+i-1,j,k)
      varsendXstt(i,j,k,7) = b2(  is+i-1,j,k)
      varsendXstt(i,j,k,8) = b3(  is+i-1,j,k)
@@ -64,9 +72,9 @@ contains
   do j=1,mgn
      varsendYend(i,j,k,1) =  d(i,je-mgn+j,k)
      varsendYend(i,j,k,2) = ei(i,je-mgn+j,k)
-     varsendYend(i,j,k,3) = v1(i,je-mgn+j,k)
-     varsendYend(i,j,k,4) = v2(i,je-mgn+j,k)
-     varsendYend(i,j,k,5) = v3(i,je-mgn+j,k)
+     varsendYend(i,j,k,nv1) = v1(i,je-mgn+j,k)
+     varsendYend(i,j,k,nv2) = v2(i,je-mgn+j,k)
+     varsendYend(i,j,k,nv3) = v3(i,je-mgn+j,k)
      varsendYend(i,j,k,6) = b1(i,je-mgn+j,k)
      varsendYend(i,j,k,7) = b2(i,je-mgn+j,k)
      varsendYend(i,j,k,8) = b3(i,je-mgn+j,k)
@@ -74,9 +82,9 @@ contains
 
      varsendYstt(i,j,k,1) =  d(i,  js+j-1,k)
      varsendYstt(i,j,k,2) = ei(i,  js+j-1,k)
-     varsendYstt(i,j,k,3) = v1(i,  js+j-1,k)
-     varsendYstt(i,j,k,4) = v2(i,  js+j-1,k)
-     varsendYstt(i,j,k,5) = v3(i,  js+j-1,k)
+     varsendYstt(i,j,k,nv1) = v1(i,  js+j-1,k)
+     varsendYstt(i,j,k,nv2) = v2(i,  js+j-1,k)
+     varsendYstt(i,j,k,nv3) = v3(i,  js+j-1,k)
      varsendYstt(i,j,k,6) = b1(i,  js+j-1,k)
      varsendYstt(i,j,k,7) = b2(i,  js+j-1,k)
      varsendYstt(i,j,k,8) = b3(i,  js+j-1,k)
@@ -91,18 +99,18 @@ contains
   do k=1,mgn
      varsendZend(i,j,k,1) =  d(i,j,ke-mgn+k)
      varsendZend(i,j,k,2) = ei(i,j,ke-mgn+k)
-     varsendZend(i,j,k,3) = v1(i,j,ke-mgn+k)
-     varsendZend(i,j,k,4) = v2(i,j,ke-mgn+k)
-     varsendZend(i,j,k,5) = v3(i,j,ke-mgn+k)
+     varsendZend(i,j,k,nv1) = v1(i,j,ke-mgn+k)
+     varsendZend(i,j,k,nv2) = v2(i,j,ke-mgn+k)
+     varsendZend(i,j,k,nv3) = v3(i,j,ke-mgn+k)
      varsendZend(i,j,k,6) = b1(i,j,ke-mgn+k)
      varsendZend(i,j,k,8) = b3(i,j,ke-mgn+k)
      varsendZend(i,j,k,9) = bp(i,j,ke-mgn+k)
 
      varsendZstt(i,j,k,1) =  d(i,j,ks+k-1  )
      varsendZstt(i,j,k,2) = ei(i,j,ks+k-1  )
-     varsendZstt(i,j,k,3) = v1(i,j,ks+k-1  )
-     varsendZstt(i,j,k,4) = v2(i,j,ks+k-1  )
-     varsendZstt(i,j,k,5) = v3(i,j,ks+k-1  )
+     varsendZstt(i,j,k,nv1) = v1(i,j,ks+k-1  )
+     varsendZstt(i,j,k,nv2) = v2(i,j,ks+k-1  )
+     varsendZstt(i,j,k,nv3) = v3(i,j,ks+k-1  )
      varsendZstt(i,j,k,6) = b1(i,j,ks+k-1  )
      varsendZstt(i,j,k,7) = b2(i,j,ks+k-1  )
      varsendZstt(i,j,k,8) = b3(i,j,ks+k-1  )
@@ -123,9 +131,9 @@ contains
   do i=1,mgn
       d(i,j,k) = varrecvXstt(i,j,k,1)
      ei(i,j,k) = varrecvXstt(i,j,k,2)
-     v1(i,j,k) = varrecvXstt(i,j,k,3)
-     v2(i,j,k) = varrecvXstt(i,j,k,4)
-     v3(i,j,k) = varrecvXstt(i,j,k,5)
+     v1(i,j,k) = varrecvXstt(i,j,k,nv1)
+     v2(i,j,k) = varrecvXstt(i,j,k,nv2)
+     v3(i,j,k) = varrecvXstt(i,j,k,nv3)
      b1(i,j,k) = varrecvXstt(i,j,k,6)
      b2(i,j,k) = varrecvXstt(i,j,k,7)
      b3(i,j,k) = varrecvXstt(i,j,k,8)
@@ -133,9 +141,9 @@ contains
      
       d(ie+i,j,k) = varrecvXend(i,j,k,1)
      ei(ie+i,j,k) = varrecvXend(i,j,k,2)
-     v1(ie+i,j,k) = varrecvXend(i,j,k,3)
-     v2(ie+i,j,k) = varrecvXend(i,j,k,4)
-     v3(ie+i,j,k) = varrecvXend(i,j,k,5)
+     v1(ie+i,j,k) = varrecvXend(i,j,k,nv1)
+     v2(ie+i,j,k) = varrecvXend(i,j,k,nv2)
+     v3(ie+i,j,k) = varrecvXend(i,j,k,nv3)
      b1(ie+i,j,k) = varrecvXend(i,j,k,6)
      b2(ie+i,j,k) = varrecvXend(i,j,k,7)
      b3(ie+i,j,k) = varrecvXend(i,j,k,8)
@@ -150,9 +158,9 @@ contains
   do j=1,mgn
       d(i,j,k) = varrecvYstt(i,j,k,1)
      ei(i,j,k) = varrecvYstt(i,j,k,2)
-     v1(i,j,k) = varrecvYstt(i,j,k,3)
-     v2(i,j,k) = varrecvYstt(i,j,k,4)
-     v3(i,j,k) = varrecvYstt(i,j,k,5)
+     v1(i,j,k) = varrecvYstt(i,j,k,nv1)
+     v2(i,j,k) = varrecvYstt(i,j,k,nv2)
+     v3(i,j,k) = varrecvYstt(i,j,k,nv3)
      b1(i,j,k) = varrecvYstt(i,j,k,6)
      b2(i,j,k) = varrecvYstt(i,j,k,7)
      b3(i,j,k) = varrecvYstt(i,j,k,8)
@@ -160,9 +168,9 @@ contains
      
       d(i,je+j,k) = varrecvYend(i,j,k,1)
      ei(i,je+j,k) = varrecvYend(i,j,k,2)
-     v1(i,je+j,k) = varrecvYend(i,j,k,3)
-     v2(i,je+j,k) = varrecvYend(i,j,k,4)
-     v3(i,je+j,k) = varrecvYend(i,j,k,5)
+     v1(i,je+j,k) = varrecvYend(i,j,k,nv1)
+     v2(i,je+j,k) = varrecvYend(i,j,k,nv2)
+     v3(i,je+j,k) = varrecvYend(i,j,k,nv3)
      b1(i,je+j,k) = varrecvYend(i,j,k,6)
      b2(i,je+j,k) = varrecvYend(i,j,k,7)
      b3(i,je+j,k) = varrecvYend(i,j,k,8)
@@ -178,9 +186,9 @@ contains
   do k=1,mgn
       d(i,j,k) = varrecvZstt(i,j,k,1)
      ei(i,j,k) = varrecvZstt(i,j,k,2)
-     v1(i,j,k) = varrecvZstt(i,j,k,3)
-     v2(i,j,k) = varrecvZstt(i,j,k,4)
-     v3(i,j,k) = varrecvZstt(i,j,k,5)
+     v1(i,j,k) = varrecvZstt(i,j,k,nv1)
+     v2(i,j,k) = varrecvZstt(i,j,k,nv2)
+     v3(i,j,k) = varrecvZstt(i,j,k,nv3)
      b1(i,j,k) = varrecvZstt(i,j,k,6)
      b2(i,j,k) = varrecvZstt(i,j,k,7)
      b3(i,j,k) = varrecvZstt(i,j,k,8)
@@ -188,9 +196,9 @@ contains
      
       d(i,j,ke+k) = varrecvZend(i,j,k,1)
      ei(i,j,ke+k) = varrecvZend(i,j,k,2)
-     v1(i,j,ke+k) = varrecvZend(i,j,k,3)
-     v2(i,j,ke+k) = varrecvZend(i,j,k,4)
-     v3(i,j,ke+k) = varrecvZend(i,j,k,5)
+     v1(i,j,ke+k) = varrecvZend(i,j,k,nv1)
+     v2(i,j,ke+k) = varrecvZend(i,j,k,nv2)
+     v3(i,j,ke+k) = varrecvZend(i,j,k,nv3)
      b1(i,j,ke+k) = varrecvZend(i,j,k,6)
      b2(i,j,ke+k) = varrecvZend(i,j,k,7)
      b3(i,j,ke+k) = varrecvZend(i,j,k,8)
@@ -213,20 +221,42 @@ subroutine XbcSendRecv(varsendXstt,varsendXend,varrecvXstt,varrecvXend)
   
   single: if(ntiles(1) == 1) then
 !$acc kernels
-!$acc loop collapse(4) independent
-  do n=1,nbc
+!$acc loop collapse(3) independent private(n)
   do k=1,kn-1
   do j=1,jn-1
   do i=1,mgn
-     ! reflection
-     varrecvXstt(i,j,k,n) = varsendXstt(mgn-i+1,j,k,n)
-     varrecvXend(i,j,k,n) = varsendXend(mgn-i+1,j,k,n)
-     ! periodic
-     !     varrecvXstt(i,j,k,n) = varsendXend(i,j,k,n)
-     !     varrecvXend(i,j,k,n) = varsendXstt(i,j,k,n)
-      varrecvXstt(i,j,k,3) = -varrecvXstt(i,j,k,3)  ! v1
-      varrecvXend(i,j,k,3) = -varrecvXend(i,j,k,3)  ! v1
-  enddo
+     select case(boundary_xin)
+     case(periodicb)
+        do n=1,nbc
+           varrecvXstt(i,j,k,n) = varsendXend(i,j,k,n)
+        enddo
+     case(reflection)
+        do n=1,nbc
+           varrecvXstt(i,j,k,n) = varsendXstt(mgn-i+1,j,k,n)
+        enddo
+        varrecvXstt(i,j,k,nv1) = -varrecvXstt(i,j,k,nv1)  ! v1
+     case(outflow)
+        do n=1,nbc
+           varrecvXstt(i,j,k,n) = varsendXstt(1,j,k,n)
+        enddo
+     end select
+     
+     select case(boundary_xout)
+     case(periodicb)
+        do n=1,nbc
+           varrecvXend(i,j,k,n) = varsendXstt(i,j,k,n)
+        enddo
+     case(reflection)
+        do n=1,nbc
+           varrecvXend(i,j,k,n) = varsendXend(mgn-i+1,j,k,n)
+        enddo
+        varrecvXend(i,j,k,nv1) = -varrecvXend(i,j,k,nv1)  ! v1
+     case(outflow)
+        do n=1,nbc
+           varrecvXend(i,j,k,n) = varsendXend(mgn,j,k,n)
+        enddo
+     end select
+     
   enddo
   enddo
   enddo
@@ -249,15 +279,21 @@ subroutine XbcSendRecv(varsendXstt,varsendXend,varrecvXstt,varrecvXend)
   else !n1dir
 
 !$acc kernels
-!$acc loop collapse(4) independent
-  do n=1,nbc
+!$acc loop collapse(3) independent private(n)
   do k=1,kn-1
   do j=1,jn-1
   do i=1,mgn
-     ! reflection
-     varrecvXstt(i,j,k,n) = varsendXstt(mgn-i+1,j,k,n)
-     varrecvXstt(i,j,k,3) = -varrecvXstt(i,j,k,3)  ! v1
-  enddo
+     select case(boundary_xin)
+     case(reflection)
+        do n=1,nbc
+           varrecvXstt(i,j,k,n) = varsendXstt(mgn-i+1,j,k,n)
+        enddo
+        varrecvXstt(i,j,k,nv1) = -varrecvXstt(i,j,k,nv1)  ! v1
+     case(outflow)
+        do n=1,nbc
+           varrecvXstt(i,j,k,n) = varsendXstt(1,j,k,n)
+        enddo
+     end select
   enddo
   enddo
   enddo
@@ -281,25 +317,30 @@ subroutine XbcSendRecv(varsendXstt,varsendXend,varrecvXstt,varrecvXend)
   else ! n1pdir
      
 !$acc kernels
-!$acc loop collapse(4) independent
-  do n=1,nbc
+!$acc loop collapse(3) independent private(n)
   do k=1,kn-1
   do j=1,jn-1
   do i=1,mgn
-     ! reflection
-     varrecvXend(i,j,k,n) = varsendXend(mgn-i+1,j,k,n)
-     varrecvXend(i,j,k,3) = -varrecvXend(i,j,k,3) ! v1   
-  enddo
+     select case(boundary_xout)
+     case(reflection)
+        do n=1,nbc
+           varrecvXend(i,j,k,n) = varsendXend(mgn-i+1,j,k,n)
+        enddo
+        varrecvXend(i,j,k,nv1) = -varrecvXend(i,j,k,nv1)  ! v1
+     case(outflow)
+        do n=1,nbc
+           varrecvXend(i,j,k,n) = varsendXend(mgn,j,k,n)
+        enddo
+     end select
   enddo
   enddo
   enddo
 !$acc end kernels
   endif n1pdir
-
-  endif single
-  
   if(nreq .ne. 0) call MPI_WAITALL ( nreq, req, stat, ierr )
   nreq = 0
+  endif single
+  
   
   return
 end subroutine XbcSendRecv
@@ -311,47 +352,123 @@ subroutine YbcSendRecv(varsendYstt,varsendYend,varrecvYstt,varrecvYend)
   real(8),dimension(in,mgn,kn,nbc),intent(out)::varrecvYstt,varrecvYend
   integer::i,j,k,n
 
-  if(ntiles(2) == 1) then
+  single: if(ntiles(2) == 1) then
 !$acc kernels
-!$acc loop collapse(4) independent
-  do n=1,nbc
+!$acc loop collapse(3) independent, private(n)
   do k=1,kn-1
   do j=1,mgn
   do i=1,in-1
-     varrecvYstt(i,j,k,n) = varsendYend(i,j,k,n)
-     varrecvYend(i,j,k,n) = varsendYstt(i,j,k,n)
-  enddo
+        
+     select case(boundary_yin)
+     case(periodicb)
+        do n=1,nbc
+           varrecvYstt(i,j,k,n) = varsendYend(i,j,k,n)
+        enddo
+     case(reflection)
+        do n=1,nbc
+           varrecvYstt(i,j,k,n) = varsendYstt(i,mgn-j+1,k,n)
+        enddo
+        varrecvYstt(i,j,k,nv2) = -varrecvYstt(i,j,k,nv2)  ! v2
+     case(outflow)
+        do n=1,nbc
+           varrecvYstt(i,j,k,n) = varsendYstt(i,1,k,n)
+        enddo
+     end select
+     
+     select case(boundary_yout)
+     case(reflection)
+        do n=1,nbc
+           varrecvYend(i,j,k,n) = varsendYend(i,mgn-j+1,k,n)
+        enddo
+        varrecvYend(i,j,k,nv2) = -varrecvYend(i,j,k,nv2)  ! v2
+     case(outflow)
+        do n=1,nbc
+           varrecvYend(i,j,k,n) = varsendYend(i,mgn,k,n)
+        enddo
+     end select
+
   enddo
   enddo
   enddo
 !$acc end kernels
-  else
+  else ! single
 
-!$acc host_data use_device(varsendYstt,varsendYend,varrecvYstt,varrecvYend)
-     nreq = nreq + 1         
-     call MPI_IRECV(varrecvYstt,mgn*in*kn*nbc &
+     n2mdir: if (n2m /= MPI_PROC_NULL) then
+!$acc host_data use_device(varsendYstt,varrecvYstt)
+        nreq = nreq + 1         
+        call MPI_IRECV(varrecvYstt,mgn*in*kn*nbc &
     & , MPI_DOUBLE &
     & , n2m, 2100, comm3d, req(nreq), ierr)
 
-     nreq = nreq + 1
-     call MPI_ISEND(varsendYstt,mgn*in*kn*nbc &
+        nreq = nreq + 1
+        call MPI_ISEND(varsendYstt,mgn*in*kn*nbc &
     & , MPI_DOUBLE &
     & , n2m, 2200, comm3d, req(nreq), ierr)
+!$acc end host_data
+     else
+!$acc kernels
+!$acc loop collapse(3) independent, private(n)
+        do k=1,kn-1
+        do j=1,mgn
+        do i=1,in-1
 
-     nreq = nreq + 1
-     call MPI_IRECV(varrecvYend,mgn*in*kn*nbc &
+           select case(boundary_yin)
+           case(reflection)
+              do n=1,nbc
+                 varrecvYstt(i,j,k,n) = varsendYstt(i,mgn-j+1,k,n)
+              enddo
+              varrecvYstt(i,j,k,nv2) = -varrecvYstt(i,j,k,nv2)  ! v2
+           case(outflow)
+              do n=1,nbc
+                 varrecvYstt(i,j,k,n) = varsendYstt(i,1,k,n)
+              enddo
+           end select
+        enddo
+        enddo
+        enddo
+!$acc end kernels
+     endif n2mdir
+     
+     n2pdir: if (n2p /= MPI_PROC_NULL) then
+!$acc host_data use_device(varsendYend,varrecvYend)
+        nreq = nreq + 1
+        call MPI_IRECV(varrecvYend,mgn*in*kn*nbc &
     & , MPI_DOUBLE &
     & , n2p,2200, comm3d, req(nreq), ierr)
 
-     nreq = nreq + 1
-     call MPI_ISEND(varsendYend,mgn*in*kn*nbc &
+        nreq = nreq + 1
+        call MPI_ISEND(varsendYend,mgn*in*kn*nbc &
     & , MPI_DOUBLE &
     & , n2p, 2100, comm3d, req(nreq), ierr)
-
+!$acc end host_data
+     else ! n2pdir
+!$acc kernels
+!$acc loop collapse(3) independent, private(n)
+        do k=1,kn-1
+        do i=1,in-1
+        do j=1,mgn
+           select case(boundary_yout)
+           case(reflection)
+              do n=1,nbc
+                 varrecvYend(i,j,k,n) = varsendYend(i,mgn-j+1,k,n)
+              enddo
+              varrecvYend(i,j,k,nv2) = -varrecvYend(i,j,k,nv2)  ! v2
+           case(outflow)
+              do n=1,nbc
+                 varrecvYend(i,j,k,n) = varsendYend(i,mgn,k,n)
+              enddo
+           end select
+     
+        enddo
+        enddo
+        enddo
+!$acc end kernels
+        
+        endif n2pdir
+     
      if(nreq .ne. 0) call MPI_WAITALL ( nreq, req, stat, ierr )
      nreq = 0
-!$acc end host_data
-  endif
+  endif single
 
   return
 end subroutine YbcSendRecv
@@ -363,47 +480,128 @@ subroutine ZbcSendRecv(varsendZstt,varsendZend,varrecvZstt,varrecvZend)
   real(8),dimension(in,jn,mgn,nbc),intent(out)::varrecvZstt,varrecvZend
   integer::i,j,k,n
   
-  if(ntiles(3) == 1) then
+  single: if(ntiles(3) == 1) then
 !$acc kernels
-!$acc loop collapse(4) independent
-  do n=1,nbc
+!$acc loop collapse(3) independent private(n)
   do k=1,mgn
   do j=1,jn-1
   do i=1,in-1
-     varrecvZstt(i,j,k,n) = varsendZend(i,j,k,n)
-     varrecvZend(i,j,k,n) = varsendZstt(i,j,k,n)
-  enddo
+         
+     select case(boundary_zin)
+     case(periodicb)
+        do n=1,nbc
+           varrecvZstt(i,j,k,n) = varsendZend(i,j,k,n)
+        enddo
+     case(reflection)
+        do n=1,nbc
+           varrecvZstt(i,j,k,n) = varsendZstt(i,j,mgn-k+1,n)
+        enddo
+        varrecvZstt(i,j,k,nv3) = -varrecvZstt(i,j,k,nv3)  ! v3
+     case(outflow)
+        do n=1,nbc
+           varrecvZstt(i,j,k,n) = varsendZstt(i,1,k,n)
+        enddo
+     end select
+     
+     select case(boundary_zout)
+     case(periodicb)
+        do n=1,nbc
+           varrecvZend(i,j,k,n) = varsendZstt(i,j,k,n)
+        enddo
+     case(reflection)
+        do n=1,nbc
+           varrecvZend(i,j,k,n) = varsendZend(i,j,mgn-k+1,n)
+        enddo
+        varrecvZend(i,j,k,nv3) = -varrecvZend(i,j,k,nv3)  ! v3
+     case(outflow)
+        do n=1,nbc
+           varrecvZend(i,j,k,n) = varsendZend(i,j,mgn,n)
+        enddo
+     end select
+
   enddo
   enddo
   enddo
 !$acc end kernels
-  else
+  else ! single
 
-!$acc host_data use_device(varsendZstt,varsendZend,varrecvZstt,varrecvZend)
-     nreq = nreq + 1         
-     call MPI_IRECV(varrecvZstt,mgn*in*jn*nbc &
+     n3mdir: if (n3m /= MPI_PROC_NULL) then
+!$acc host_data use_device(varsendZstt,varrecvZstt)
+        nreq = nreq + 1         
+        call MPI_IRECV(varrecvZstt,mgn*in*jn*nbc &
     & , MPI_DOUBLE &
     & , n3m, 3100, comm3d, req(nreq), ierr)
 
-     nreq = nreq + 1
-     call MPI_ISEND(varsendZstt,mgn*in*jn*nbc &
+        nreq = nreq + 1
+        call MPI_ISEND(varsendZstt,mgn*in*jn*nbc &
     & , MPI_DOUBLE &
     & , n3m, 3200, comm3d, req(nreq), ierr)
-
-     nreq = nreq + 1
-     call MPI_IRECV(varrecvZend,mgn*in*jn*nbc &
+!$acc end host_data
+     else
+        
+!$acc kernels
+!$acc loop collapse(3) independent private(n)
+        do k=1,mgn
+        do j=1,jn-1
+        do i=1,in-1
+         
+           select case(boundary_zin)
+           case(reflection)
+              do n=1,nbc
+                 varrecvZstt(i,j,k,n) = varsendZstt(i,j,mgn-k+1,n)
+              enddo
+              varrecvZstt(i,j,k,nv3) = -varrecvZstt(i,j,k,nv3)  ! v3
+           case(outflow)
+              do n=1,nbc
+                 varrecvZstt(i,j,k,n) = varsendZstt(i,1,k,n)
+              enddo
+           end select
+    
+        enddo
+        enddo
+        enddo
+!$acc end kernels
+     endif n3mdir
+     
+     n3pdir: if (n3p /= MPI_PROC_NULL) then
+!$acc host_data use_device(varsendZend,varrecvZend)
+        nreq = nreq + 1
+        call MPI_IRECV(varrecvZend,mgn*in*jn*nbc &
     & , MPI_DOUBLE &
     & , n3p, 3200, comm3d, req(nreq), ierr)
 
-     nreq = nreq + 1
-     call MPI_ISEND(varsendZend,mgn*in*jn*nbc &
+        nreq = nreq + 1
+        call MPI_ISEND(varsendZend,mgn*in*jn*nbc &
     & , MPI_DOUBLE &
     & , n3p, 3100, comm3d, req(nreq), ierr)
-
+!$acc end host_data
+     else
+        !$acc kernels
+!$acc loop collapse(3) independent private(n)
+        do j=1,jn-1
+        do i=1,in-1
+        do k=1,mgn
+        
+           select case(boundary_zout)
+           case(reflection)
+              do n=1,nbc
+                 varrecvZend(i,j,k,n) = varsendZend(i,j,mgn-k+1,n)
+              enddo
+              varrecvZend(i,j,k,nv3) = -varrecvZend(i,j,k,nv3)  ! v3
+           case(outflow)
+              do n=1,nbc
+                 varrecvZend(i,j,k,n) = varsendZend(i,j,mgn,n)
+              enddo
+           end select
+           
+  enddo
+  enddo
+  enddo
+!$acc end kernels
+     endif n3pdir
      if(nreq .ne. 0) call MPI_WAITALL ( nreq, req, stat, ierr )
      nreq = 0
-!$acc end host_data
-  endif
+  endif single
 
   return
 end subroutine ZbcSendRecv
